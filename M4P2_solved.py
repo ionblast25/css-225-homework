726ec221f1623d1c0866bf54b5ec501ff11a639f
ionblast25 <72461342+ionblast25@users.noreply.github.com>
2020-10-22 08:43:39 -0500

    Add files via upload

---
+++ M4P2_solved.py
@@ -0,0 +1,9 @@
+#This program will take a password from the user and print an appropriate message
+#The 'in' keyword checks to see if a value exists somewhere in the given string.
+
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting == ("All pirates must die!"):
+        print("Greetings, hater of pirates!")	
+else:
+        print("Go away, pirate.")
+        
---
+++ M4P4_solved.py
@@ -0,0 +1,169 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P1.py =================
+What is the current time (in hours 0-23)?5
+How many hours do you want to wait?9
+14
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P1.py =================
+What is the current time (in hours 0-23)?24
+How many hours do you want to wait?53
+77
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?Arrr!
+Go away, pirate.
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?hi
+Greetings, hater of pirates!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?d
+Greetings, hater of pirates!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?kajf
+Go away, pirate.
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?All pirates must die!
+Greetings, hater of pirates!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?p
+Greetings, hater of pirates!
+>>> 
+Warning (from warnings module):
+  File "C:\Users\Justine\Downloads\M4P2.py", line 5
+    if greeting is ("All pirates must die!"):
+SyntaxWarning: "is" with a literal. Did you mean "=="?
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?s
+Go away, pirate.
+>>> 
+Warning (from warnings module):
+  File "C:\Users\Justine\Downloads\M4P2.py", line 5
+    if greeting is ("All pirates must die!"):
+SyntaxWarning: "is" with a literal. Did you mean "=="?
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?All pirates must die!
+Go away, pirate.
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?j
+Go away, pirate.
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P2.py =================
+Hello, possible pirate! What's the password?All pirates must die!
+Greetings, hater of pirates!
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> lazy
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    lazy
+NameError: name 'lazy' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> happy
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    happy
+NameError: name 'happy' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+r
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================ RESTART: C:\Users\Justine\Downloads\M4P3 (1).py ===============
+>>> red
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    red
+NameError: name 'red' is not defined
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 1999
+That's totally the present!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 1900
+Woah, that's the past!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 28172
+That's totally the present!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 213123
+That's totally the present!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 1922
+Far out, that's the future!!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 287421
+That's totally the present!
+>>> 
+================== RESTART: C:\Users\Justine\Downloads\M4P4.py =================
+Greetings! What is your year of origin? 8214798234
+Far out, that's the future!!
+>>> 
